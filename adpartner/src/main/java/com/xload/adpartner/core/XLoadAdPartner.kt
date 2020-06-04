e31775516aff55c797755e85ed09265a55e380be
John Paul Cas <test.com>
2020-06-04 13:00:46 +0800

    Add is development methods

--- adpartner/src/main/java/com/xload/adpartner/core/XLoadAdPartner.kt
+++ adpartner/src/main/java/com/xload/adpartner/core/XLoadAdPartner.kt
@@ -13,37 +13,52 @@ object XLoadAdPartner {
 
     val LOG = XLoadAdPartner::class.java.simpleName
 
-    fun init(DEV_KEY: String, application: Application, listener: XLoadAdPartnerListener) {
+    fun init(
+        DEV_KEY: String,
+        application: Application,
+        isDevelopment: Boolean,
+        listener: XLoadAdPartnerListener
+    ) {
         val appsFlyerListener = object : AppsFlyerConversionListener {
             override fun onAppOpenAttribution(conversionData: MutableMap<String, String>?) {
-                conversionData.let {
-                    for (attrName in it!!.keys) {
-                        Log.d(
-                            LOG,
-                            "attribute: " + attrName + " = " + conversionData?.get(attrName)
-                        )
+                if (isDevelopment) {
+                    conversionData.let {
+                        for (attrName in it!!.keys) {
+                            Log.d(
+                                LOG,
+                                "attribute: " + attrName + " = " + conversionData?.get(attrName)
+                            )
+                        }
                     }
                 }
             }
 
             override fun onConversionDataSuccess(conversionData: MutableMap<String, Any>?) {
                 listener.convertionDataSuccess()
-                conversionData.let {
-                    for (attrName in it!!.keys) {
-                        Log.d(
-                            LOG,
-                            "attribute: " + attrName + " = " + conversionData?.get(attrName)
-                        )
+                if (isDevelopment) {
+                    conversionData.let {
+                        for (attrName in it!!.keys) {
+                            Log.d(
+                                LOG,
+                                "attribute: " + attrName + " = " + conversionData?.get(attrName)
+                            )
+                        }
                     }
                 }
             }
 
             override fun onConversionDataFail(errMessage: String?) {
                 listener.convertionDataError("Error Message[ConversionDataFail]: ${errMessage}")
+                if (isDevelopment) {
+                    Log.d(LOG, "onversionDataFail message: ${errMessage}")
+                }
             }
 
             override fun onAttributionFailure(errMessage: String?) {
                 listener.convertionDataError("Error Message[AttributionFailure]: ${errMessage}")
+                if (isDevelopment) {
+                    Log.d(LOG, "AttributionFailure message: ${errMessage}")
+                }
             }
         }
 
